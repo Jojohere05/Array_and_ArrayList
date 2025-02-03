43022c681e7ea47720e2dbb29472d0e73f6683e1
Jojohere05 <2005jhotikaraja@gmail.com>
2025-02-03 11:49:16 +0530

    userinput-input for array

--- UserInput.java
+++ UserInput.java
@@ -1,12 +1,14 @@
 import java.util.*;
 class UserInput{
+	Scanner scan = new Scanner(System.in);
 	int[] arrayInput(){
 		int[] array = new int[5];
 		System.out.println("enter 5 nos");
-		Scanner scan = new Scanner(System.in);
+		
 		for (int i=0;i<array.length;i++){
 			array[i]= scan.nextInt();
 		}
 	return array;
 	}
+
 }
