53ca0a97c80571b13c0d9a929c7b6063a9a2ca4a
Jojohere05 <2005jhotikaraja@gmail.com>
2025-02-03 11:37:18 +0530

    Array to arraylist

--- ArrayFunction.java
+++ ArrayFunction.java
@@ -38,4 +38,12 @@ class ArrayFunction{
         
         return minIndex;
     }
+	ArrayList<Integer> arrayToArrayList(int[] array){
+		ArrayList<Integer> list= new ArrayList<Integer>();
+		for (int i=0;i<array.length;i++){
+			list.add(array[i]);
+		}
+		return list;
+	}
+
 }
